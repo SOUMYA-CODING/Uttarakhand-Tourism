c706b4e7a8d63e4645230af0b703974fc4f6cc19
Soumya Prakash Sahu <52891015+SNH-CODING@users.noreply.github.com>
2022-07-03 23:17:56 +0530

    Uttarakhand Tourism App

--- lib/user_section/dashboard_screen.dart
+++ lib/user_section/dashboard_screen.dart
@@ -1,3 +1,6 @@
+import 'dart:ui';
+
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
@@ -13,6 +16,33 @@ class _DashboardScreenState extends State<DashboardScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
+      bottomNavigationBar: BottomNavigationBar(
+        selectedItemColor: Colors.white,
+        unselectedItemColor: Colors.blueGrey,
+        showUnselectedLabels: false,
+        backgroundColor: Colors.blueAccent,
+        type: BottomNavigationBarType.fixed,
+        unselectedFontSize: 0,
+        selectedFontSize: 0,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.house_fill),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.ticket_fill),
+            label: 'My Bookings',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.person_solid),
+            label: 'Profile',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.phone_fill),
+            label: 'Contact Us',
+          ),
+        ],
+      ),
       body: SafeArea(
         child: Padding(
           padding: const EdgeInsets.fromLTRB(20.0, 20.0, 20.0, 0.0),
@@ -105,7 +135,18 @@ class _DashboardScreenState extends State<DashboardScreen> {
                       child: Ink.image(
                         image: const NetworkImage(
                             'https://static.toiimg.com/thumb/msid-90946619,imgsize-190588,width-400,resizemode-4/90946619.jpg'),
-                        height: 110.0,
+                        width: 260.0,
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                    Card(
+                      clipBehavior: Clip.antiAliasWithSaveLayer,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                      child: Ink.image(
+                        image: const NetworkImage(
+                            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT6VZe62cKjEj597wX8EPewhi5tUlagVWHKAw&usqp=CAU'),
                         width: 260.0,
                         fit: BoxFit.cover,
                       ),
@@ -118,22 +159,284 @@ class _DashboardScreenState extends State<DashboardScreen> {
                       child: Ink.image(
                         image: const NetworkImage(
                             'https://static.toiimg.com/thumb/msid-90946619,imgsize-190588,width-400,resizemode-4/90946619.jpg'),
-                        height: 110.0,
                         width: 260.0,
                         fit: BoxFit.cover,
                       ),
                     ),
+                  ],
+                ),
+              ),
+              const SizedBox(
+                height: 15.0,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text(
+                    'Popular Places',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  TextButton(
+                    onPressed: () {},
+                    child: const Text('see more'),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 10.0,
+              ),
+              SizedBox(
+                height: 250.0,
+                width: double.infinity,
+                child: ListView(
+                  scrollDirection: Axis.horizontal,
+                  children: [
                     Card(
+                      elevation: 4,
+                      // Change this
+                      shadowColor: Colors.black,
                       clipBehavior: Clip.antiAliasWithSaveLayer,
                       shape: RoundedRectangleBorder(
                         borderRadius: BorderRadius.circular(10.0),
                       ),
-                      child: Ink.image(
-                        image: const NetworkImage(
-                            'https://static.toiimg.com/thumb/msid-90946619,imgsize-190588,width-400,resizemode-4/90946619.jpg'),
-                        height: 110.0,
-                        width: 260.0,
-                        fit: BoxFit.cover,
+                      child: Padding(
+                        padding: const EdgeInsets.all(6.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Container(
+                              width: 170.0,
+                              height: 180.0,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(10.0),
+                                image: const DecorationImage(
+                                  image: AssetImage('assets/photo1.jpg'),
+                                  fit: BoxFit.cover,
+                                  repeat: ImageRepeat.noRepeat,
+                                ),
+                              ),
+                            ),
+                            const SizedBox(
+                              height: 10.0,
+                            ),
+                            const Text(
+                              'Kedarnath',
+                              style: TextStyle(
+                                fontWeight: FontWeight.w800,
+                                fontSize: 18.0,
+                              ),
+                            ),
+                            RichText(
+                              text: const TextSpan(
+                                children: [
+                                  WidgetSpan(
+                                    child: Icon(
+                                      Icons.location_on,
+                                      size: 16,
+                                      color: Color(0XFF304352),
+                                    ),
+                                  ),
+                                  TextSpan(
+                                    text: " Rudraprayag district",
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.w800,
+                                      fontSize: 16.0,
+                                      color: Color(0XFF304352),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    Card(
+                      elevation: 4,
+                      // Change this
+                      shadowColor: Colors.black,
+                      clipBehavior: Clip.antiAliasWithSaveLayer,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(6.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Container(
+                              width: 170.0,
+                              height: 180.0,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(10.0),
+                                image: const DecorationImage(
+                                  image: AssetImage('assets/img_three.jpg'),
+                                  fit: BoxFit.cover,
+                                  repeat: ImageRepeat.noRepeat,
+                                ),
+                              ),
+                            ),
+                            const SizedBox(
+                              height: 10.0,
+                            ),
+                            const Text(
+                              'Kedarnath',
+                              style: TextStyle(
+                                fontWeight: FontWeight.w800,
+                                fontSize: 18.0,
+                              ),
+                            ),
+                            RichText(
+                              text: const TextSpan(
+                                children: [
+                                  WidgetSpan(
+                                    child: Icon(
+                                      Icons.location_on,
+                                      size: 16,
+                                      color: Color(0XFF304352),
+                                    ),
+                                  ),
+                                  TextSpan(
+                                    text: " Rudraprayag district",
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.w800,
+                                      fontSize: 16.0,
+                                      color: Color(0XFF304352),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    Card(
+                      elevation: 4,
+                      // Change this
+                      shadowColor: Colors.black,
+                      clipBehavior: Clip.antiAliasWithSaveLayer,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(6.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Container(
+                              width: 170.0,
+                              height: 180.0,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(10.0),
+                                image: const DecorationImage(
+                                  image: AssetImage('assets/img_three.jpg'),
+                                  fit: BoxFit.cover,
+                                  repeat: ImageRepeat.noRepeat,
+                                ),
+                              ),
+                            ),
+                            const SizedBox(
+                              height: 10.0,
+                            ),
+                            const Text(
+                              'Kedarnath',
+                              style: TextStyle(
+                                fontWeight: FontWeight.w800,
+                                fontSize: 18.0,
+                              ),
+                            ),
+                            RichText(
+                              text: const TextSpan(
+                                children: [
+                                  WidgetSpan(
+                                    child: Icon(
+                                      Icons.location_on,
+                                      size: 16,
+                                      color: Color(0XFF304352),
+                                    ),
+                                  ),
+                                  TextSpan(
+                                    text: " Rudraprayag district",
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.w800,
+                                      fontSize: 16.0,
+                                      color: Color(0XFF304352),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    Card(
+                      elevation: 4,
+                      // Change this
+                      shadowColor: Colors.black,
+                      clipBehavior: Clip.antiAliasWithSaveLayer,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                      child: Padding(
+                        padding: const EdgeInsets.all(6.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Container(
+                              width: 170.0,
+                              height: 180.0,
+                              decoration: BoxDecoration(
+                                borderRadius: BorderRadius.circular(10.0),
+                                image: const DecorationImage(
+                                  image: AssetImage('assets/img_three.jpg'),
+                                  fit: BoxFit.cover,
+                                  repeat: ImageRepeat.noRepeat,
+                                ),
+                              ),
+                            ),
+                            const SizedBox(
+                              height: 10.0,
+                            ),
+                            const Text(
+                              'Kedarnath',
+                              style: TextStyle(
+                                fontWeight: FontWeight.w800,
+                                fontSize: 18.0,
+                              ),
+                            ),
+                            RichText(
+                              text: const TextSpan(
+                                children: [
+                                  WidgetSpan(
+                                    child: Icon(
+                                      Icons.location_on,
+                                      size: 16,
+                                      color: Color(0XFF304352),
+                                    ),
+                                  ),
+                                  TextSpan(
+                                    text: " Rudraprayag district",
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.w800,
+                                      fontSize: 16.0,
+                                      color: Color(0XFF304352),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
                       ),
                     ),
                   ],
